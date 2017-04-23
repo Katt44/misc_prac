752687d7cfb2cadd33e7412ce91e9a96d6f3d14a
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-22 18:21:46 -0700

    function practice

---
+++ function_prac.py
@@ -0,0 +1,13 @@
+
+
+#method practice
+
+
+def name_game(name):
+	myname = "katt"
+	if name == myname.lower():
+		print ("Hey thats my name!!")
+	else:
+		print ("hey {} thats a good name! My name is {}.".format(name,myname))
+
+name_game("ratt")
