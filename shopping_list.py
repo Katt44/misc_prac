3ed6624d88395734c6112f5b7658d892cb12abbf
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-22 18:22:24 -0700

    shopping list first commit

---
+++ shopping_list.py
@@ -0,0 +1,12 @@
+#Shopping_list
+
+
+
+
+#make a list to hold items
+# print out instructions how to use app
+#ask for new items
+#add new items to our list
+#be able to quit the app
+#print out list
+
